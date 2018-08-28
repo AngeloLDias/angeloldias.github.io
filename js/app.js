f606cb92c9761a06d4342ff5fd7f4622ea6b7b3c
Angelo Lima <angelolimadias@gmail.com>
2018-08-28 20:18:51 -0300

    alterando back

--- js/app.js
+++ js/app.js
@@ -3,9 +3,22 @@
 //     x.style.width = "64px";
 // }    
 
-        function alterarfundo(){
+        function alterarfundoicon13(){
             //Ao posicionar o cursor sobre a div
             $('.backredes').css('background-image', 'url(../img/backlinkdin.png');
+            $('.backredes').css('background-size', 'cover');
+             $('.icon-13,.icon-15, .icon-17, .icon-26').css('transition', '0.5s');
+            // $('.icon-13').css('animation', 'skew 3s infinite');
+            // $('.icon-13').css('animation-direction', 'alternate');
+            // $('.backredes').css('transform', 'skewX(25deg)');
+            // $('.backredes').css('transform', 'skewX(25deg)');
+            // $('.backredes').css('transform', 'skewX(25deg)');
+            // $('.backredes').css('transform', 'skewX(25deg)');
+
+        };
+        function alterarfundoicon15(){
+            //Ao posicionar o cursor sobre a div
+            $('.backredes').css('background', 'red');
             $('.backredes').css('background-size', 'cover');
              $('.icon-13,.icon-15, .icon-17, .icon-26').css('transition', '0.5s');
             // $('.icon-13').css('animation', 'skew 3s infinite');
